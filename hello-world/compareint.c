8190dd37b2a24a246d46cf46dafdaf390550f71e
hessam_khoobkar <amirhessam.dev@gmail.com>
2024-02-12 18:21:35 +0330

    Compare two user inputed numers

--- hello-world/compareint.c
+++ hello-world/compareint.c
@@ -5,9 +5,27 @@ int main(void)
   // init x and y
   int x;
   int y;
-  // get x
-  // get y
-  // compare
-  // report back
+
+  // get x and y
+  printf("Please inter first number: ");
+  scanf("%i", &x);
+  printf("Please inter second number: ");
+  scanf("%i", &y);
+
+  // compare and report back
+  if (x > y)
+  {
+    printf("Number %i is greater than %i\n", x, y);
+  }
+  else if (x < y)
+  {
+    printf("Number %i is lesser than %i\n", x, y);
+  }
+  else
+  {
+    printf("Number %i and %i are equel\n", x, y);
+  }
+
+  // Exit
   return 0;
 }
