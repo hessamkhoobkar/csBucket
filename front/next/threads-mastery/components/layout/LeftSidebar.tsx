316c34e26ea09901c61e3a8af10dc3e34b05c0e1
hessam_khoobkar <amirhessam.dev@gmail.com>
2024-03-17 15:59:00 +0330

    closing this and moving to pitlane fever design

--- front/next/threads-mastery/components/layout/LeftSidebar.tsx
+++ front/next/threads-mastery/components/layout/LeftSidebar.tsx
@@ -1,21 +1,41 @@
-import { sidebarLinks } from "@/constants";
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname, useRouter } from "next/navigation";
+
+import { sidebarLinks } from "@/constants";
 
 export default function LeftSidebar() {
+  // const router = useRouter();
+  const pathname = usePathname();
+
   return (
     <section className="custom-scrollbar sticky left-0 top-0 z-20 flex h-screen w-fit flex-col justify-between overflow-auto border-r border-r-dark-4 bg-dark-2 pb-5 pt-28 max-md:hidden">
       <div className="flex w-full flex-1 flex-col gap-6 px-6">
-        {sidebarLinks.map((link) => (
-          <Link
-            key={link.route}
-            href={link.route}
-            className="relative flex justify-start gap-4 rounded-lg p-4"
-          >
-            <Image src={link.imgURL} alt={link.label} width={24} height={24} />
-            <span className="max-lg:hidden">{link.label}</span>
-          </Link>
-        ))}
+        {sidebarLinks.map((link) => {
+          const isActive =
+            (pathname.includes(link.route) && link.route.length > 1) ||
+            pathname === link.route;
+          return (
+            <Link
+              key={link.route}
+              href={link.route}
+              className={`relative flex justify-start gap-4 rounded-lg p-4 ${
+                isActive && "bg-violet-700 text-white"
+              }`}
+            >
+              <Image
+                className="text-white"
+                src={link.imgURL}
+                alt={link.label}
+                width={24}
+                height={24}
+              />
+              <span className="max-lg:hidden">{link.label}</span>
+            </Link>
+          );
+        })}
       </div>
     </section>
   );
