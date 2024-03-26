f43d7f2c555c5492fca8e4ae0a260a6626d64cba
hessam_khoobkar <amirhessam.dev@gmail.com>
2024-03-27 00:15:16 +0330

    Add POST endpoint and update server port

--- back/javascript/express-beginner/app.js
+++ back/javascript/express-beginner/app.js
@@ -1,14 +1,22 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.status(200).send('Hello World');
+})
+
+app.post('/', (req, res) => {
+  console.log(req.body);
+  res.status(200).json(req.body);
 })
 
 app.all('*', (req, res) => {
   res.status(404).send('Not Found');
 })
 
-app.listen(6019, () => {
-  console.log('Server is running on port 6019...');
+app.listen(8090, () => {
+  console.log(`localhost:8090`);
+  console.log('Server is running on port 8090...');
 })
