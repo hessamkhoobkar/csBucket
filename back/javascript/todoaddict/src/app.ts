3ed0dd7db2c471ded6c8dc284220568260973f31
hessam_khoobkar <amirhessam.dev@gmail.com>
2024-03-27 04:39:57 +0330

    setup mongodb and mongoose

--- back/javascript/todoaddict/src/app.ts
+++ back/javascript/todoaddict/src/app.ts
@@ -2,13 +2,21 @@ import express, { Express } from "express";
 import dotenv from "dotenv";
 import { router as tasks } from "./routers/tasks";
 
+const connectDB = require("./db/connection");
+
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.json());
+
 app.use("/api/v1/tasks", tasks);
 
-app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
-});
+connectDB()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`[server]: Server is running at http://localhost:${port}`);
+    });
+  })
+  .catch(console.dir);
